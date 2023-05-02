ec8c91c5826a6407c6ab283b3667bcc10f64977a
Ravindu007 <ravindu0504@gmail.com>
2023-05-02 17:52:56 +0530

    new baseurl 2

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -12,7 +12,7 @@ export const useLogin = () =>{
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch(process.env.REACT_APP_BASEURL  + "/api/user/login",{
+    const response = await fetch("https://travelog-backend.onrender.com/api/user/login",{
       method:"POST",
       headers:{'Content-Type':'application/json'},
       body:JSON.stringify({email, password})
